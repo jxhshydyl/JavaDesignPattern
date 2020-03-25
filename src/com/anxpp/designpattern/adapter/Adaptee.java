87747f9150b3dffa7ab98dc55732f24ee998db2a
liuweipeng <dad9479@dingtalk.com>
2020-03-25 12:12:40 +0800

    修改

--- src/com/anxpp/designpattern/adapter/Adaptee.java
+++ src/com/anxpp/designpattern/adapter/Adaptee.java
@@ -1,5 +1,5 @@
 package com.anxpp.designpattern.adapter;
-//被适配者
+//被适配者 适配者类
 class Adaptee{
 	void playMp3(Object src){
 		System.out.println("播放MP3：" + src);
--- src/com/anxpp/designpattern/builder/TestUse.java
+++ src/com/anxpp/designpattern/builder/TestUse.java
@@ -9,7 +9,6 @@ public class TestUse {
 		System.out.println(builder.date());
 		builder = new DateBuilder2(date).buildDate(2066, 3, 5);
 		System.out.println(builder.date());
-
 		//使用指挥者
 		IDateBuilder iDateBuilder1=new DateBuilder1(date);
 		Derector derector1=new Derector(iDateBuilder1);
@@ -19,7 +18,7 @@ public class TestUse {
 		IDateBuilder iDateBuilder2=new DateBuilder2(date);
 		Derector derector2=new Derector(iDateBuilder2);
 		String date2 = derector2.getDate(2066, 3, 5);
-		System.out.println(date1);
+		System.out.println(date2);
 
 	}
 }
